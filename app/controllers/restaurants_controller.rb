3242f6705022e513187e814c2648a7dbeb88bea7
antoine schmitt <antoine.schmitt@gmail.com>
2019-08-14 17:54:37 +0200

    correction

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,4 @@
 class RestaurantsController < ApplicationController
-  # before_action :set_restaurant, only: [:show]
-
   def index
     @restaurants = Restaurant.all
   end
@@ -19,17 +17,13 @@ class RestaurantsController < ApplicationController
     if @restaurant.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render "new"
     end
   end
 
   private
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
-  end
-
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :category)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,18 @@
 class ReviewsController < ApplicationController
-  before_action :find_restaurant, only: [:new, :create]
-  def new
-    @review = Review.new
-  end
-
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render "restaurants/show"
     end
   end
 
   private
 
-  def find_restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
-  end
-
   def review_params
-    params.require(:review).permit(:content, :rating)
+    params.require(:review).permit(:rating, :content)
   end
 end
